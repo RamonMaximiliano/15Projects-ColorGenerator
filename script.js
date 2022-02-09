04fdaddb935f1ada740e6b64cbdb892837d0b91f
Ramon <ramon_max1986@hotmail.com>
2022-02-09 10:48:11 -0300

    Finished

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-{/* <p class="color">Color code = 0123456789ABCDEF</p> */}
+
+function changeColor() {
+
+    let colorCode = '0123456789ABCDEF';
+    let arrayColor = [...colorCode]
+    console.log(arrayColor)
+    let finalColor = '#'
+    for (x = 0; x < 6; x++) {
+        finalColor += arrayColor[Math.floor(Math.random() * arrayColor.length)];
+    }
+
+    document.querySelector("p.color").innerHTML = finalColor
+    document.body.style.backgroundColor = finalColor
+
+}
